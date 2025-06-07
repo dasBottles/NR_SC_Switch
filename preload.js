3089679ebed39930c6e6f71e36eb6a9b4d03c735
dasaniii <arattanasack@gmail.com>
2025-06-07 10:10:53 -0700

    fix config bug

--- preload.js
+++ preload.js
@@ -2,13 +2,11 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  readConfig: () => ipcRenderer.invoke('read-config'),
-  writeConfig: data => ipcRenderer.invoke('write-config', data),
-  readNrscSettings: () => ipcRenderer.invoke('read-nrsc-settings'),
+  readConfig:        () => ipcRenderer.invoke('read-config'),
+  writeConfig:       data => ipcRenderer.invoke('write-config', data),
+  readNrscSettings:  () => ipcRenderer.invoke('read-nrsc-settings'),
   updatePlayerCount: count => ipcRenderer.invoke('update-player-count', count),
-  runEnvSwitch: env => ipcRenderer.invoke('run-env-switch', env),
-  handleLaunch: (env) => {
-    console.log('🔔 preload.handleLaunch called with', env);
-    return ipcRenderer.invoke('run-env-switch', env);
-  }  // … any other methods you already had …
+  runEnvSwitch:      env   => ipcRenderer.invoke('run-env-switch', env),
+  handleLaunch:      env   => ipcRenderer.invoke('run-env-switch', env)
+
 });
